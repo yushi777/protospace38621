bbffa36fdc66a7ab487050b659956f3b7e29003f
yushi777 <ucy77777@gmail.com>
2023-01-13 18:40:07 +0900

    prototypeコントローラーの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
   
 
   def index
